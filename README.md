b1d45e89e4b7a244052966fed35595a93c11c00a
Gabriel Alves Cunha <gabriel925486@gmail.com>
2020-01-14 18:48:38 -0300

    jogando arquivos.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Just-Learning-CSS
-css little projects
---
+++ airpods/app.js
@@ -0,0 +1,45 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLL MAGIC
+const controller = new ScrollMagic.Controller();
+
+//scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+//TEXT ANIMATION
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//VIDEO ANIMATION
+let accamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accamount;
+
+  video.currentTime = delay;
+}, 33.3333);
